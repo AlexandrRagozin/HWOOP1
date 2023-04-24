e586ac92ef2626757f9739d4f9b26d719a6115ed
Alexandr <ragozzzzinalexandr@gmail.com>
2023-04-24 17:45:09 +0500

    added drinks

---
+++ drink.java
@@ -0,0 +1,13 @@
+public class drink extends Product {
+    private int volume;
+    
+    protected Drinks(String name, int cost, int quantity, String measure, int volume) {
+        super(name, cost, quantity, measure);
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;        
+    }
+    
+}
