763709bd1047d7e7a152de976d4f50f592656732
Alexandr <ragozzzzinalexandr@gmail.com>
2023-04-24 22:07:27 +0500

    added forchildren

---
+++ forchildren.java
@@ -0,0 +1,18 @@
+public class forchildren extends Product {
+    private int minAge;
+    private String hypoallergenic;
+    protected forchildren(String name, int cost, int quantity, String measure, int minAge, String hypoallergenic) {
+        super(name, cost, quantity, measure);
+        this.minAge = minAge;
+        this.hypoallergenic = hypoallergenic;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
+
+    public String getHypoallergenic() {
+        return hypoallergenic;
+    }
+    
+}
